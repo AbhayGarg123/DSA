7470608e0d745d9903be789efd057dc4bde02936
Abhay Garg <154878156+AbhayGarg123@users.noreply.github.com>
2025-06-18 21:00:18 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // check number is even or odd
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println(num + " is an odd number.");
+        }
+
+        // check the person(He/She) is eligible to vote or not
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+        sc.close(); 
+}
+}
