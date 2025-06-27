815e3a963e743f2adc31f6976647ddccf57eb641
Abhay Garg <154878156+AbhayGarg123@users.noreply.github.com>
2025-06-27 22:23:44 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,26 @@
+// print pattern 3 
+// * * * * 
+//   * * *
+//     * *
+//       *
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        
+        for (int i = 1; i <= rows; i++) {
+    
+            for (int j = 1; j <i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = rows-i+1; k >0; k--) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+    }
+}
---
+++ pattern4.java
@@ -0,0 +1,26 @@
+//print pattern4 
+// * * * * 
+// * * *   
+// * *     
+// *  
+import java.util.Scanner;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            
+             for (int k = rows-i+1; k >0; k--) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <i; j++) {
+                System.out.print("  ");
+            }
+            System.out.println(); 
+        }
+    }
+}
