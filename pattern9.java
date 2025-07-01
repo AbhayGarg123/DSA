205fe911cb9132106a2d8dcf6975ca8c3d6a866c
Abhay Garg <154878156+AbhayGarg123@users.noreply.github.com>
2025-07-01 21:35:08 +0530

    Add files via upload

---
+++ pattern9.java
@@ -0,0 +1,22 @@
+// print pattern 9
+// *
+//   *
+//     *
+//       *
+import java.util.Scanner;
+public class pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        
+        for (int i = 0; i < rows; i++) {
+          for(int j=0; j<i;j++) {
+                System.out.print("  ");
+            }
+            System.out.println("*"); 
+        }
+        sc.close();
+    }
+}
