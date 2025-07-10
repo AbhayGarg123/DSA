3d0a862bf0076c0056feabda40036a234735362d
Abhay Garg <154878156+AbhayGarg123@users.noreply.github.com>
2025-07-10 22:35:30 +0530

    Add files via upload

---
+++ subtractionarray.java
@@ -0,0 +1,31 @@
+// difference of two arrays array1-array2
+import java.util.Scanner;
+
+public class subtractionarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the arrays: ");
+        int size = sc.nextInt();
+
+        int[] array1 = new int[size];
+        int[] array2 = new int[size];
+        int[] sumArray = new int[size];
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < size; i++) {
+            array1[i] = sc.nextInt();
+        }
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < size; i++) {
+            array2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            sumArray[i] = array1[i] - array2[i];
+        }
+        System.out.println("Resultant array after addition:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(sumArray[i] + " ");
+        }
+
+        sc.close();
+    }
+}
