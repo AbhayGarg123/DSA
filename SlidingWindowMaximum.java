2c3a38dfff1a94d6b8cda95eedaec1e6fc22b01d
Abhay Garg <154878156+AbhayGarg123@users.noreply.github.com>
2025-07-20 18:18:21 +0530

    Add files via upload

---
+++ SlidingWindowMaximum.java
@@ -0,0 +1,49 @@
+// Sliding window maximum problem
+import java.util.*;
+
+public class SlidingWindowMaximum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.print("Enter window size k: ");
+        int k = sc.nextInt();
+
+        int[] result = maxSlidingWindow(nums, k);
+
+        System.out.println("Maximum of each sliding window:");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k <= 0) return new int[0];
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+}
